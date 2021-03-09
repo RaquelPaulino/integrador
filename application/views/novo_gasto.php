4b4c50b39ebe124f42bcf16b056240d5e067faa4
jana8964e <janaise.g@escolar.ifrn.edu.br>
2021-03-09 20:40:52 -0300

    Modificações na aula

---
+++ application/views/novo_gasto.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Novo Gasto</h1>
+    <form action="">
+    <label for="data"> Data: <input type="date" name="data"> </label><br/>
+    <label for="descrição"> Descrição: <input type="text" name="descrição"> </label><br/> 
+    <label for="valor"> Valor: <input type="number" name="valor"> </label><br/>
+    <button type="button"> Enviar</button>
+    <button type="resert"> Limpar</button>
+    </form>
+
+
+</body>
+</html>
