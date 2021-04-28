188a760bc0472491c8b4013965a4df7b198374fa
jana8964e <janaise.g@escolar.ifrn.edu.br>
2021-04-27 22:32:35 -0300

    mudei

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => '',
-	'password' => '',
-	'database' => '',
+	'hostname' => 'profaalbalopes.info',
+	'username' => 'profaa16_cardapi',
+	'password' => '!}3#i{%0~K]g',
+	'database' => 'profaa16_cardapioifrn',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
